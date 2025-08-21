cec573e9ba881f4f597c3df6124cd1658f5118a1
Carlos Obando <136123266+Z0MB13KV21@users.noreply.github.com>
2025-08-20 21:33:43 -0600

    Add files via upload

--- Catalogos/KVS.php
+++ Catalogos/KVS.php
@@ -274,7 +274,7 @@ a.navbar-brand{
 
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #02b5bd;">
     <div class="container">
-        <a class="navbar-brand">KVSublimacion</a>
+        <a class="navbar-brand">PAVSublimacion</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
